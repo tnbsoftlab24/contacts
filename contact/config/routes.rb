1b71a577e7233a5a9bb2bf30aa3cb520e5664d23
Hervé Houndjetodé <vetcho006@hotmail.com>
2022-09-04 11:05:07 +0100

    Refactored Code.

--- contact/config/routes.rb
+++ contact/config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   # authenticate do
-  root to: "contact#index"
+  root to: "contacts#index"
   resources :contacts
 end
